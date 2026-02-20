5085c6669b4513f0ce00ca2bc625f47ea2f037c4
JITENDER <156467292+jitenderkumardas@users.noreply.github.com>
2026-02-20 16:17:51 +0530

    Update robots.ts
    changes sitemap url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: "/",
             disallow: "/private/",
         },
-        sitemap: "https://example.com/sitemap.xml",
+        sitemap: "https://antiportfolio-theta.vercel.app/sitemap.xml",
     };
 }
